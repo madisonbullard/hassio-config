dd463d32215df84935b332e13ed04d2cbc004f09
Madison Bullard <madison.bullard@gmail.com>
2021-02-04 21:56:56 -0500

    update badnest

--- custom_components/badnest/__init__.py
+++ custom_components/badnest/__init__.py
@@ -3,7 +3,14 @@ import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
 
 from .api import NestAPI
-from .const import DOMAIN, CONF_ISSUE_TOKEN, CONF_COOKIE, CONF_USER_ID, CONF_ACCESS_TOKEN, CONF_REGION
+from .const import (
+    DOMAIN,
+    CONF_ISSUE_TOKEN,
+    CONF_COOKIE,
+    CONF_USER_ID,
+    CONF_ACCESS_TOKEN,
+    CONF_REGION,
+)
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -17,7 +24,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_ISSUE_TOKEN, default=""): cv.string,
                 vol.Required(CONF_COOKIE, default=""): cv.string,
                 vol.Optional(CONF_REGION, default="us"): cv.string,
-            }
+            },
         )
     },
     extra=vol.ALLOW_EXTRA,
@@ -40,13 +47,7 @@ def setup(hass, config):
         region = None
 
     hass.data[DOMAIN] = {
-        'api': NestAPI(
-            user_id,
-            access_token,
-            issue_token,
-            cookie,
-            region,
-        ),
+        "api": NestAPI(user_id, access_token, issue_token, cookie, region,),
     }
 
     return True
